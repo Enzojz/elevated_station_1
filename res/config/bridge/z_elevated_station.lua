0e3006917586ed8d1a12867163d5552c6b3e67e9
Enzojz <enzojz@gmail.com>
2021-07-12 01:05:16 +0200

    2.0

--- res/config/bridge/z_elevated_station.lua
+++ res/config/bridge/z_elevated_station.lua
@@ -1,21 +1,115 @@
 local path = "station/train/passenger/elevated_station/"
 function data()
-	return {
-		name = _("Elevated Station Viaduct"),
-
-		yearFrom = 1990,
-		yearTo = 0,
-
-		carriers = { "RAIL" },
-
-		pillarBase =   { path.."pillar_btm.mdl"},
-		pillarRepeat = { path.."pillar_btm.mdl"},
-		pillarTop =    { path.."pillar_top.mdl"},
-
-		railingBegin =  { path.."railing_rep_side.mdl",   path.."railing_rep_rep.mdl" },
-		railingRepeat = { path.."railing_rep_side.mdl",   path.."railing_rep_rep.mdl" },
-		railingEnd =    { path.."railing_rep_side.mdl",   path.."railing_rep_rep.mdl" },
-
-		cost = 540.0,
-	}
+    return {
+        name = _("Elevated Station"),
+        
+        yearFrom = 1990,
+        yearTo = 0,
+        
+        carriers = {"RAIL"},
+        
+        speedLimit = 100,
+        
+        pillarLen = 3,
+        
+        
+        pillarMinDist = 45.0,
+        pillarMaxDist = 66.0,
+        pillarTargetDist = 50.0,
+        
+        ignoreWaterCollision = true,
+        cost = 540.0,
+        
+        updateFn = function(params)
+            local result = {
+                railingModels = {},
+                pillarModels = {}
+            }
+            
+            for i, height in ipairs(params.pillarHeights) do
+                local colHeight = height - 10.2
+                local nSeg = math.ceil(colHeight / 6)
+                
+                local rs = {
+                    {
+                        {
+                            id = path .. "pillar_top.mdl",
+                            transf = {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, height, 1}
+                        }
+                    }
+                }
+                
+                for s = 1, nSeg do
+                    table.insert(
+                        rs,
+                        {
+                            {
+                                id = path .. "pillar_btm.mdl",
+                                transf = {1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, -10.2 - (s - 1) * 6 + height, 1}
+                            }
+                        }
+                )
+                end
+                
+                table.insert(result.pillarModels, rs)
+            
+            end
+            
+            for i, interval in ipairs(params.railingIntervals) do
+                local nSeg = math.floor((interval.length) / 6)
+                if nSeg < 1 then nSeg = 1 end
+                local lSeg = interval.length / nSeg
+                local xScale = lSeg / 6
+                
+                local minOffset = interval.lanes[1].offset
+                local maxOffset = interval.lanes[#interval.lanes].offset
+                
+                local sp = params.railingWidth - (maxOffset - minOffset) - 2
+                
+                local width = maxOffset - minOffset
+                local nPart = math.floor(width / 5)
+                local wPart = width / nPart
+                local yScale = wPart / 5
+                
+                local set = function(n)
+                    local x = (n - 1) * lSeg
+                    
+                    local set = {}
+                    if interval.lanes[1].type ~= 2 and interval.lanes[1].type ~= 3 then
+                        table.insert(set,
+                            {
+                                id = path .. "railing_rep_side_1.mdl",
+                                transf = {xScale, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, x, minOffset - sp, 0, 1}
+                            })
+                    end
+                    if interval.lanes[#interval.lanes].type ~= 1 and interval.lanes[#interval.lanes].type ~= 3 then
+                        table.insert(set,
+                            {
+                                id = path .. "railing_rep_side_2.mdl",
+                                transf = {xScale, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, x, maxOffset + sp, 0, 1}
+                            })
+                    end
+                    
+                    for k = 1, nPart do
+                        table.insert(set,
+                            {
+                                id = path .. "railing_rep_rep.mdl",
+                                transf = {xScale, 0, 0, 0, 0, yScale, 0, 0, 0, 0, 1, 0, x, minOffset + k * wPart, 0, 1}
+                            }
+                    )
+                    end
+                    
+                    return set
+                end
+                
+                local rs = {}
+                for s = 1, nSeg do
+                    table.insert(rs, set(s))
+                end
+                table.insert(result.railingModels, rs)
+            end
+            
+            return result
+        end
+    }
 end
--- res/config/track/zzz_mock_elevated_station.lua
+++ res/config/track/zzz_mock_elevated_station.lua
@@ -9,38 +9,41 @@ function data()
 
 	t.shapeWidth = 4.0
 	t.shapeStep = 4.0
-	t.shapeSleeperStep = 8.0 / 12.0
+	t.shapeSleeperStep = 8.0 / 13.0
 
 	t.ballastHeight = .3
 	t.ballastCutOff = .1
 
 	t.sleeperBase = t.ballastHeight
-	t.sleeperLength = .26
+	t.sleeperLength = .24
 	t.sleeperWidth = 2.6
 	t.sleeperHeight = .08
-	t.sleeperCutOff = .02
+	t.sleeperCutOff = .04
 
 	t.railTrackWidth = 1.435
 	t.railBase = t.sleeperBase + t.sleeperHeight
 	t.railHeight = .15
 	t.railWidth = .07
 	t.railCutOff = .02
+    
+    t.embankmentSlopeLow = 0.75
+    t.embankmentSlopeHigh = 2.5
 
 	t.catenaryBase = 5.917 + t.railBase + t.railHeight
 	t.catenaryHeight = 1.35
+	t.catenaryPoleDistance = 25.0
 
 	t.trackDistance = 5.0
 
-	t.speedLimit = 120.0 / 3.6
-	t.speedCoeffs = { .85, 30.0, .6 }		-- curve speed limit = a * (radius + b) ^ c
+	t.speedLimit = 5.0 / 3.6
+	t.speedCoeffs = { .9, 15.0, .63 }
 
 	t.ballastMaterial = "track/ballast.mtl"
 	t.sleeperMaterial = "track/transparent.mtl"
 	t.railMaterial = "track/transparent.mtl"
 	t.catenaryMaterial = "track/catenary.mtl"
-	t.trackMaterial = "track/transparent.mtl"
-	t.tunnelWallMaterial = "track/tunnel_ug.mtl"
-	t.tunnelHullMaterial = "track/tunnel_hull.mtl"
+	t.tunnelWallMaterial = "track/transparent.mtl"
+	t.tunnelHullMaterial = "track/transparent.mtl"
 
 	t.catenaryPoleModel = "railroad/power_pole_2.mdl"
 	t.catenaryMultiPoleModel = "railroad/power_pole_1.mdl"
@@ -50,6 +53,18 @@ function data()
 	t.bumperModel = "station/train/passenger/elevated_station/platform_end.mdl"
 	t.switchSignalModel = "railroad/switch_box.mdl"
 
+	t.fillGroundTex = "ballast_fill_stone.lua"
+	t.borderGroundTex = "ballast.lua"
+
+	t.railModel ="station/train/passenger/elevated_station/void.mdl"
+	t.sleeperModel = "station/train/passenger/elevated_station/void.mdl"
+	t.trackStraightModel = {
+		"station/train/passenger/elevated_station/void.mdl",
+		"station/train/passenger/elevated_station/void.mdl",
+		"station/train/passenger/elevated_station/void.mdl",
+		"station/train/passenger/elevated_station/void.mdl",
+	}
+	
 	t.cost = 0.0
 
 	return t
